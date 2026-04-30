d5bbf9fb79169b882dd596432ddb1334c5428152
Yugo <yugosakamoto0717@gmail.com>
2026-04-29 21:23:22 -0300

    Implements simple app for tests to be made

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Dividing by 0");
+    }
+
+    return a/b;
+}
+
+export {sum, subtract, multiply, divide};
